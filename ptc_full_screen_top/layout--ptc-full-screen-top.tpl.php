f986ac28e172401bd8b86385a224eb94b694d84f
Kristin Snelling <kristin@palantetech.coop>
2020-01-09 19:51:52 -0500

    Changes to top layout

--- ptc_full_screen_top/layout--ptc-full-screen-top.tpl.php
+++ ptc_full_screen_top/layout--ptc-full-screen-top.tpl.php
@@ -2,7 +2,7 @@
 /**
  * @file
  * Template for a full screen layout with the header menu at the top of the screen.
- * Supportive themes will have a theme setting for l-big_statement and juiced-main class areas to have a full screen background image
+ * Supportive themes will have a theme setting for l-big_statement and ptc-main class areas to have a full screen background image
  *
  * Variables:
  * - $title: The page title, for use in the actual HTML content.
@@ -18,12 +18,13 @@
  *   region of the layout. This layout supports the following divs:
  */
 ?>
-<div class="layout--ptc-full-screen-top container <?php print implode(' ', $classes); ?>"<?php print backdrop_attributes($attributes); ?>>
+<div class="layout--ptc-full-screen-top container js <?php print implode(' ', $classes); ?>"<?php print backdrop_attributes($attributes); ?>>
+  <div class="loader"></div>
   <div id="skip-link">
     <a href="#main-content" class="element-invisible element-focusable"><?php print t('Skip to main content'); ?></a>
   </div>
 
-  <main class="l-wrapper container juiced-main" role="main" aria-label="<?php print t('Main content'); ?>">
+  <main class="l-wrapper container ptc-main" role="main" aria-label="<?php print t('Main content'); ?>">
       <a id="main-content"></a>
 
     <?php if ($content['header']): ?>
@@ -31,83 +32,84 @@
       <?php print $content['header']; ?>
     </header>
     <?php endif; ?>
+    
+    <?php if ($content['top']): ?>
+    <div class="l-top col-12 container gutters" role="region">
+      <?php print $content['top']; ?>
+    </div>
+    <?php endif; ?>
+    
+    <?php if ($content['statement1']): ?>
+    <div class="l-statement l-statement1 col-12 container gutters section justify-center" role="region">
+        <?php print $content['statement1']; ?>
+    </div>
+    <?php endif; ?>
 
-    <div class="l-content col-12">
-        <?php if ($messages): ?>
-          <div class="l-messages" role="status" aria-label="<?php print t('Status messages'); ?>">
-            <?php print $messages; ?>
-          </div>
-        <?php endif; ?>
+    <?php if ($content['secondary1']): ?>
+    <div class="l-secondary l-secondary1 col-12 container secondary gutters" role="region">
+        <?php print $content['secondary1']; ?>
+    </div>
+    <?php endif; ?>
 
+    
+    <?php if ($content['calltoaction']): ?>
+    <div class="l-calltoaction col-12 container gutters section" role="secondary" aria-label="<?php print t('Action to take'); ?>">
+       <?php print $content['calltoaction']; ?>
+    </div>
+    <?php endif; ?>
+    
+    <div class="l-content container gutters col-12 section">
       <?php print render($title_prefix); ?>
-      <?php if ($title): ?>
-        <h1 class="page-title">
-          <?php print $title; ?>
-        </h1>
+      <div class="header col-12 container gutters">
+        <?php if ($title): ?>
+          <h1 class="page-title col-10 align-center">
+            <?php print $title; ?>
+          </h1>
+        <?php endif; ?>
+        <?php print render($title_suffix); ?>
+      </div>
+      <?php if ($messages): ?>
+      <div class="l-messages col-10 align-center" role="status" aria-label="<?php print t('Status messages'); ?>">
+        <?php print $messages; ?>
+      </div>
       <?php endif; ?>
-      <?php print render($title_suffix); ?>
-
+      <?php print $action_links; ?>
+      <?php print $content['content']; ?>
       <?php if ($tabs): ?>
-        <nav class="tabs small-12 column" role="tablist" aria-label="<?php print t('Admin Content Navigation Tabs'); ?>">
+        <nav class="tabs col-10 align-center column" role="tablist" aria-label="<?php print t('Admin Content Navigation Tabs'); ?>">
           <?php print $tabs; ?>
         </nav>
       <?php endif; ?>
-
-      <?php print $action_links; ?>
-      <?php print $content['content']; ?>
     </div>
-  </main>
-
-  <?php if ($content['top']): ?>
-      <div class="l-top col-12" role="region">
-      <?php print $content['top']; ?>
-      </div>
-  <?php endif; ?>
-
-  <?php if ($content['statement1']): ?>
-      <div class="l-statement l-statement1 col-12" role="region">
-        <?php print $content['statement1']; ?>
-      </div>
-  <?php endif; ?>
-
-  <?php if ($content['secondary1']): ?>
-      <div class="l-secondary l-secondary1 col-12" role="region">
-        <?php print $content['secondary1']; ?>
-      </div>
-  <?php endif; ?>
-
-  <?php if ($content['statement2']): ?>
-      <div class="l-statement l-statement2 col-12" role="region">
+  
+    <?php if ($content['statement2']): ?>
+      <div class="l-statement l-statement2 col-12 container section" role="region">
         <?php print $content['statement2']; ?>
       </div>
-  <?php endif; ?>
+    <?php endif; ?>
 
-  <?php if ($content['secondary2']): ?>
-      <div class="l-secondary l-secondary2 col-12" role="region">
+    <?php if ($content['secondary2']): ?>
+      <div class="l-secondary l-secondary2 col-12 container gutters secondary" role="region">
         <?php print $content['secondary2']; ?>
       </div>
-  <?php endif; ?>
+    <?php endif; ?>
 
-  <?php if ($content['statement3']): ?>
-      <div class="l-statement l-statement3 col-12" role="region">
+    <?php if ($content['statement3']): ?>
+      <div class="l-statement l-statement3 col-12 container gutters section" role="region">
         <?php print $content['statement3']; ?>
       </div>
-  <?php endif; ?>
-
-  <?php if ($content['bottom']): ?>
-    <div class="l-bottom col-12" role="region">
+    <?php endif; ?>
+  
+    <?php if ($content['bottom']): ?>
+    <div class="l-bottom col-12 container section" role="region">
         <?php print $content['bottom']; ?>
     </div>
-  <?php endif; ?>
+    <?php endif; ?>
 
-  <?php if ($content['calltoaction']): ?>
-      <footer class="l-calltoaction col-12" role="secondary" aria-label="<?php print t('Action to take'); ?>">
-        <?php print $content['calltoaction']; ?>
-      </footer>
-  <?php endif; ?>
+  </main>
 
   <?php if ($content['footer']): ?>
-    <footer class="l-footer col-12" role="contentinfo" aria-label="<?php print t('Footer navigation'); ?>">
+    <footer class="l-footer col-12 container gutters secondary" role="contentinfo" aria-label="<?php print t('Footer navigation'); ?>">
       <?php print $content['footer']; ?>
     </footer>
   <?php endif; ?>
